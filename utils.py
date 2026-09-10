9d2c62cdae2461c72c6eacc4140b05c37e7a8bd5
SIMON OHURE OCHITI <145343940+pharynixoku@users.noreply.github.com>
2026-09-10 01:50:54 -0700

    Update utils.py

---
+++ utils.py
@@ -0,0 +1,6 @@
+def square(n): """Return the square of a number."""
+    return n ** 2
+def is_even(n): """Return True if the number is even, otherwise False."""
+    return n % 2 == 0 
+def celsius_to_fahrenheit(c): """Convert Celsius to Fahrenheit.""" 
+      return (c * 9 / 5) + 32
